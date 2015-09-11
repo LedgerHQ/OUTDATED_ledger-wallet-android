937a4c3282e6a59097f46889b0e81d204c9e61d2
Nessnkr <julien.benoit.fr@gmail.com>
2015-09-11 17:08:03 +0200

    Edit: PIN selection and confirmation

--- app/src/main/scala/com/ledger/ledgerwallet/app/unplugged/UnpluggedPINChoiceActivity.scala
+++ app/src/main/scala/com/ledger/ledgerwallet/app/unplugged/UnpluggedPINChoiceActivity.scala
@@ -35,27 +35,97 @@ import android.os.Bundle
 import android.view._
 import com.ledger.ledgerwallet.R
 import com.ledger.ledgerwallet.base.{BaseActivity, BaseFragment}
+import com.ledger.ledgerwallet.utils.TR
+import com.ledger.ledgerwallet.common._
+import com.ledger.ledgerwallet.widget.{TextView, PinTextView}
+import android.view.inputmethod.EditorInfo
+import android.util.Log
+
 
 class UnpluggedPINChoiceActivity extends BaseActivity {
   override def onCreate(savedInstanceState: Bundle): Unit = {
     super.onCreate(savedInstanceState)
+
+    val intent = getIntent()
+
+    val bundle = new Bundle()
+
+    intent.hasExtra("pinCode") match {
+      case true =>
+        val pinCode = intent.getStringExtra("pinCode")
+        Log.v("PIN Code", pinCode)
+        bundle.putString("pinCode", pinCode)
+        bundle.putString("pinChoiceStep", "confirmPIN")
+      case _ =>
+        bundle.putString("pinChoiceStep", "enterPIN")
+    }
+
     setContentView(R.layout.single_fragment_holder_activity)
 
+    val fragment = new UnpluggedPINChoiceActivityContentFragment()
+    fragment.setArguments(bundle)
+
     getSupportFragmentManager
-        .beginTransaction()
-        .replace(R.id.fragment_container, new UnpluggedPINChoiceActivityContentFragment())
-        .commitAllowingStateLoss()
+      .beginTransaction()
+      .replace(R.id.fragment_container, fragment)
+      .commitAllowingStateLoss()
   }
 }
 
+
+
 class UnpluggedPINChoiceActivityContentFragment extends BaseFragment {
 
-	override def onCreateView(inflater: LayoutInflater, container: ViewGroup, savedInstanceState: Bundle): View = {
+  lazy val pinTextView = TR(R.id.pin_view).as[PinTextView]
+  lazy val button = TR(R.id.button).as[TextView]
+  lazy val alert = TR(R.id.alert).as[TextView]
+
+  lazy val pinChoiceStep = getArguments().getString("pinChoiceStep");
+
+  override def onCreateView(inflater: LayoutInflater, container: ViewGroup, savedInstanceState: Bundle): View = {
     inflater.inflate(R.layout.unplugged_pin_choice_fragment, container, false)
   }
 
   override def onViewCreated(view: View, savedInstanceState: Bundle): Unit = {
     super.onViewCreated(view, savedInstanceState)
-    
+
+    pinTextView.requestFocus()
+    pinTextView.setFocusableInTouchMode(true)
+    pinTextView.setOnEditorActionListener((actionId: Int, event: KeyEvent) => {
+      actionId match {
+        case EditorInfo.IME_ACTION_NEXT =>
+          if (pinTextView.getText().length() == 4) {
+            pinChoiceStep match {
+              case "enterPIN" =>
+                button onClick {
+                  val intent = new Intent(getActivity, classOf[UnpluggedPINChoiceActivity])
+                  intent.putExtra("pinCode", pinTextView.getText().toString)
+                  startActivity(intent)
+                }
+                button.setVisibility(View.VISIBLE)
+
+              case _ =>
+                if (pinTextView.getText().toString == getArguments().getString("pinCode")) {
+                  alert.setVisibility(View.INVISIBLE)
+
+                  button onClick {
+                    val intent = new Intent(getActivity, classOf[UnpluggedPINChoiceActivity])
+                    intent.putExtra("pinCode", pinTextView.getText().toString)
+                    startActivity(intent)
+                  }
+                  button.setVisibility(View.VISIBLE)
+                } else {
+                  alert.setVisibility(View.VISIBLE)
+                }
+            }
+          }else{
+            button.setVisibility(0)
+          }
+          true
+        case _ => false
+      }
+    })
+
   }
+
 }
